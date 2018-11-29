e4c655c68b7838f8ed71a29a1bc8f2d700f939da
Leverson Carlos <lcjohnny@hotmail.com>
2018-11-29 12:12:51 -0200

    just a fancy log system

--- test/Program.cs
+++ test/Program.cs
@@ -15,25 +15,25 @@ namespace StreamZIP
       {
          try
          {
-            Console.WriteLine("Initialized");
+            Log("Initialized");
             var httpUrl = "https://onedrive.live.com/download.aspx?cid=E6710A9CD086B950&authKey=%21AJeGdFhgxPdH8M4&resid=E6710A9CD086B950%2157128&ithint=%2Ecbz";
 
             // MAKE A HTTP CALL REQUIRING ONLY THE HEADERS SO IT SHOULD BE A FAST CALL
             var httpClient = GetHttpClient();
             var httpMessage = await httpClient.GetAsync(httpUrl, HttpCompletionOption.ResponseHeadersRead);
-            if (!httpMessage.IsSuccessStatusCode) { Console.WriteLine($"Http Message Status Code: {httpMessage.StatusCode}"); return; }
+            if (!httpMessage.IsSuccessStatusCode) { Log($"Received Http Status Code {httpMessage.StatusCode}."); return; }
 
             // READ THE CONTENT SIZE FROM THE RETURNED HEADERS
             var contentLength = httpMessage.Content.Headers
                .GetValues("Content-Length")
                .Select(x => long.Parse(x))
                .FirstOrDefault();
-            Console.WriteLine($"ContentLength: {contentLength}");
+            Log($"The full content will have {contentLength} bytes");
 
             return;
          }
-         catch (Exception ex) { Console.WriteLine($"Exception: {ex.ToString()}"); }
-         finally { Console.WriteLine("Finalized"); }
+         catch (Exception ex) { Log($"Exception: {ex.ToString()}"); }
+         finally { Log("Finalized"); }
       }
 
       private static HttpClient GetHttpClient()
@@ -42,6 +42,9 @@ namespace StreamZIP
          httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
          return httpClient;
       }
+
+      private static void Log(string value)
+      { Console.WriteLine($"{DateTime.Now.ToString("mm:ss.fff")} - {value}"); }
 
    }
 }
