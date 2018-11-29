19f2c1b03f0312cbd100280565af62d319fa4350
Leverson Carlos <lcjohnny@hotmail.com>
2018-11-29 14:41:11 -0200

    routine to locate start of the central directory starting from the end of stream

--- test/Program.cs
+++ test/Program.cs
@@ -18,10 +18,12 @@ namespace StreamZIP
             Log("Initialized");
             var httpUrl = "https://onedrive.live.com/download.aspx?cid=E6710A9CD086B950&authKey=%21AJeGdFhgxPdH8M4&resid=E6710A9CD086B950%2157128&ithint=%2Ecbz";
 
+
             // MAKE A HTTP CALL REQUIRING ONLY THE HEADERS SO IT SHOULD BE A FAST CALL
             var httpClient = GetHttpClient();
             var httpMessage = await httpClient.GetAsync(httpUrl, HttpCompletionOption.ResponseHeadersRead);
             if (!httpMessage.IsSuccessStatusCode) { Log($"Received Http Status Code {httpMessage.StatusCode}."); return; }
+
 
             // READ THE CONTENT SIZE FROM THE RETURNED HEADERS
             var contentLength = httpMessage.Content.Headers
@@ -29,6 +31,48 @@ namespace StreamZIP
                .Select(x => long.Parse(x))
                .FirstOrDefault();
             Log($"The full content will have {contentLength} bytes");
+
+
+            // TRY TO LOCATE THE CENTRAL DIRECTORY STARTING FROM THE END OF THE FILE AND GOING BACK BY 256 BYTES EACH TIME
+            long directoryStart = -1;
+            short locateTries = 1;
+            long locateStart = contentLength - 22;
+            long locateFinish = contentLength;
+            var locateChunkSize = 256;
+            while (directoryStart == -1 && locateTries <= 4)
+            {
+               Log($"Trying to found central directory. {locateTries} try");
+
+               // MAKE A HTTP CALL USING THE RANGE HEADER
+               locateStart -= (locateChunkSize * locateTries);
+               httpClient = new HttpClient();
+               httpClient.DefaultRequestHeaders.Range = new RangeHeaderValue(locateStart, locateFinish);
+               var byteArray = await httpClient.GetByteArrayAsync(httpUrl);
+
+               // TRY TO LOCATE THE CENTRAL DIRECTORY STARTING DEFINED BY
+               // 50 4B 05 06
+               long pos = (byteArray.Length - 22);
+               while (pos >= 0)
+               {
+
+                  if (byteArray[pos + 0] == 0x50 &&
+                      byteArray[pos + 1] == 0x4b &&
+                      byteArray[pos + 2] == 0x05 &&
+                      byteArray[pos + 3] == 0x06)
+                  {
+                     directoryStart = pos;
+                     break; // FOUND IT
+                  }
+                  else { pos--; }
+               }
+
+               locateTries++;
+            }
+
+            if (directoryStart == -1) { Log($"Hasnt found the central directory."); return; }
+            Log($"Found directory starting at {directoryStart}");
+
+
 
             return;
          }
