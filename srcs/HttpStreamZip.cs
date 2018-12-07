2830b3a1e96da26087804b0d394fa16efe3e5c3c
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-07 17:15:58 -0200

    http client

--- srcs/HttpStreamZip.cs
+++ srcs/HttpStreamZip.cs
@@ -1,8 +1,19 @@
 ﻿using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 
 namespace System.IO.Compression
 {
    public class HttpStreamZip
    {
+
+      HttpClient httpClient { get; set; }
+      public HttpStreamZip() : this(new HttpClient()) { }
+      public HttpStreamZip(HttpClient httpClient)
+      {
+         this.httpClient = httpClient;
+         this.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
+      }
+
    }
 }
