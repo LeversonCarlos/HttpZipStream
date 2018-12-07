69f6497164768729a00748115194676a6980523d
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-07 18:16:23 -0200

    central directory wip

--- srcs/HttpStreamZip.cs
+++ srcs/HttpStreamZip.cs
@@ -9,20 +9,22 @@ namespace System.IO.Compression
    public class HttpStreamZip
    {
 
+      string httpUrl { get; set; }
       HttpClient httpClient { get; set; }
-      public HttpStreamZip() : this(new HttpClient()) { }
-      public HttpStreamZip(HttpClient httpClient)
+      public HttpStreamZip(string httpUrl) : this(httpUrl, new HttpClient()) { }
+      public HttpStreamZip(string httpUrl, HttpClient httpClient)
       {
+         this.httpUrl = httpUrl;
          this.httpClient = httpClient;
          this.httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/octet-stream"));
       }
 
       long ContentLength { get; set; }
-      private async Task<bool> RefreshContentLengthAsync(string httpUrl)
+      private async Task<bool> RefreshContentLengthAsync()
       {
          try
          {
-            var httpMessage = await this.httpClient.GetAsync(httpUrl, HttpCompletionOption.ResponseHeadersRead);
+            var httpMessage = await this.httpClient.GetAsync(this.httpUrl, HttpCompletionOption.ResponseHeadersRead);
             if (!httpMessage.IsSuccessStatusCode) { return false; }
             this.ContentLength = httpMessage.Content.Headers
                .GetValues("Content-Length")
@@ -38,16 +40,49 @@ namespace System.IO.Compression
       {
          try
          {
-            this.directoryData = new DirectoryData { Offset = -1 };
 
-            // TRY TO FOUND THE CENTRAL DIRECTORY FOUR TIMES SLOWLY INCREASING THE BUFFER SIZE
+            // INITIALIZE
+            this.directoryData = new DirectoryData { Offset = -1 };
+            var secureMargin = 22;
+            var chunkSize = 256;
+            var rangeStart = this.ContentLength - secureMargin;
+            var rangeFinish = this.ContentLength;
+
+            // TRY TO FOUND THE CENTRAL DIRECTORY FOUR TIMES SLOWLY INCREASING THE CHUNK SIZE
             short tries = 1;
             while (this.directoryData.Offset == -1 && tries <= 4)
             {
 
+               // MAKE A HTTP CALL USING THE RANGE HEADER
+               rangeStart -= (chunkSize * tries);
+               this.httpClient.DefaultRequestHeaders.Range = new RangeHeaderValue(rangeStart, rangeFinish);
+               var byteArray = await httpClient.GetByteArrayAsync(this.httpUrl);
+
+               // TRY TO LOCATE THE END OF CENTRAL DIRECTORY DEFINED BY
+               // 50 4B 05 06
+               // https://en.wikipedia.org/wiki/Zip_(file_format)#End_of_central_directory_record_(EOCD)
+               int pos = (byteArray.Length - secureMargin);
+               while (pos >= 0) { 
+
+                  // FOUND CENTRAL DIRECTORY 
+                  if (byteArray[pos + 0] == 0x50 &&
+                      byteArray[pos + 1] == 0x4b &&
+                      byteArray[pos + 2] == 0x05 &&
+                      byteArray[pos + 3] == 0x06)
+                  {
+                     // this.directoryData.Size = ByteArrayToInt(byteArray, pos + 12);
+                     // this.directoryData.Offset = ByteArrayToInt(byteArray, pos + 16);
+                     // this.directoryData.Entries = ByteArrayToShort(byteArray, pos + 10);
+                     return true;
+                  }
+                  else { pos--; }
+
+               }
+
                tries++;
             }
 
+            return false;
          }
          catch (Exception) { throw; }
       }
