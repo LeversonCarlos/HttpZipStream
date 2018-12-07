d2d423b10b0f6469bf6b00d96600366522d5364a
Leverson Carlos <lcjohnny@hotmail.com>
2018-12-07 18:35:17 -0200

    starting the test project

---
+++ test/HttpStreamZipTest.cs
@@ -0,0 +1,16 @@
+using System;
+using Xunit;
+
+namespace System.IO.Compression
+{
+   public class HttpStreamZipTest
+   {
+
+      [Fact]
+      public void FullTest()
+      {
+         Assert.Equal(4, 4);
+      }
+
+   }
+}
